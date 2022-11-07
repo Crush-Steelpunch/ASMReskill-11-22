6605d5f252a60092f9624411e584f34fcbaa653d
Leon L. Robinson <leon.robinson@qa.com>
2022-11-07 12:33:37 +0000

    passing data to jinja templates

--- application/routes.py
+++ application/routes.py
@@ -6,6 +6,7 @@ from flask import render_template
 
 @app.route('/', methods = ['GET'])
 def read():
-#    pystaff = Staff.query.join(Subjects).first()
-    return render_template('disclaimer.html')
+    pystaff = Staff.query.join(Subjects).first()
+    breakpoint()
+    return render_template('front.html', jistaff=pystaff )
 
--- runapp.py
+++ runapp.py
@@ -1,5 +1,4 @@
 from application import app
-from application.route import read
 
 if __name__=='__main__':
     app.run(debug=True, host='0.0.0.0', port=5000)
